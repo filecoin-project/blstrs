3a05da377bff8be02fdfc4d6c1a4ac12bc771ca1
dignifiedquire <me@dignifiedquire.com>
2021-01-19 19:49:54 +0100

    feat: add hash-to-curve and small utils needed in bls-signatures

--- src/g1.rs
+++ src/g1.rs
@@ -63,6 +63,18 @@ impl From<G1Projective> for G1Affine {
     }
 }
 
+impl AsRef<blst_p1_affine> for G1Affine {
+    fn as_ref(&self) -> &blst_p1_affine {
+        &self.0
+    }
+}
+
+impl AsMut<blst_p1_affine> for G1Affine {
+    fn as_mut(&mut self) -> &mut blst_p1_affine {
+        &mut self.0
+    }
+}
+
 impl Eq for G1Affine {}
 impl PartialEq for G1Affine {
     #[inline]
@@ -377,6 +389,18 @@ impl fmt::Display for G1Projective {
     }
 }
 
+impl AsRef<blst_p1> for G1Projective {
+    fn as_ref(&self) -> &blst_p1 {
+        &self.0
+    }
+}
+
+impl AsMut<blst_p1> for G1Projective {
+    fn as_mut(&mut self) -> &mut blst_p1 {
+        &mut self.0
+    }
+}
+
 impl From<&G1Affine> for G1Projective {
     fn from(p: &G1Affine) -> G1Projective {
         let mut out = blst_p1::default();
@@ -591,6 +615,23 @@ impl G1Projective {
     /// Returns the z coordinate.
     pub fn z(&self) -> Fp {
         Fp(self.0.z)
+    }
+
+    /// Hash to curve algorithm.
+    pub fn hash_to_curve(msg: &[u8], dst: &[u8], aug: &[u8]) -> Self {
+        let mut res = Self::zero();
+        unsafe {
+            blst_hash_to_g1(
+                &mut res.0,
+                msg.as_ptr(),
+                msg.len(),
+                dst.as_ptr(),
+                dst.len(),
+                aug.as_ptr(),
+                aug.len(),
+            );
+        }
+        res
     }
 }
 
--- src/pairing.rs
+++ src/pairing.rs
@@ -12,3 +12,108 @@ pub fn pairing(p: G1Affine, q: G2Affine) -> Fp12 {
 
     out.into()
 }
+
+macro_rules! impl_pairing {
+    ($name:ident, $p:ty, $q:ty, $aggregate:ident, $aggregated:ident) => {
+        /// Aggregate pairings efficiently.
+        #[derive(Debug)]
+        pub struct $name {
+            v: Box<[u64]>,
+        }
+
+        impl $name {
+            pub fn new(hash_or_encode: bool, dst: &[u8]) -> Self {
+                let v: Vec<u64> = vec![0; unsafe { blst_pairing_sizeof() } / 8];
+                let mut obj = Self {
+                    v: v.into_boxed_slice(),
+                };
+                obj.init(hash_or_encode, dst);
+                obj
+            }
+
+            pub fn init(&mut self, hash_or_encode: bool, dst: &[u8]) {
+                unsafe { blst_pairing_init(self.ctx(), hash_or_encode, dst.as_ptr(), dst.len()) }
+            }
+
+            fn ctx(&mut self) -> *mut blst_pairing {
+                self.v.as_mut_ptr() as *mut blst_pairing
+            }
+
+            fn const_ctx(&self) -> *const blst_pairing {
+                self.v.as_ptr() as *const blst_pairing
+            }
+
+            pub fn aggregate(
+                &mut self,
+                pk: &$p,
+                sig: Option<&$q>,
+                msg: &[u8],
+                aug: &[u8],
+            ) -> Result<(), BLST_ERROR> {
+                let res = unsafe {
+                    $aggregate(
+                        self.ctx(),
+                        &pk.0,
+                        match sig {
+                            Some(sig) => &sig.0,
+                            None => std::ptr::null(),
+                        },
+                        msg.as_ptr(),
+                        msg.len(),
+                        aug.as_ptr(),
+                        aug.len(),
+                    )
+                };
+                if res == BLST_ERROR::BLST_SUCCESS {
+                    Ok(())
+                } else {
+                    Err(res)
+                }
+            }
+
+            pub fn aggregated(gtsig: &mut Fp12, sig: &$q) {
+                unsafe { $aggregated(&mut gtsig.0, &sig.0) }
+            }
+
+            pub fn commit(&mut self) {
+                unsafe { blst_pairing_commit(self.ctx()) }
+            }
+
+            pub fn merge(&mut self, ctx1: &Self) -> Result<(), BLST_ERROR> {
+                let res = unsafe { blst_pairing_merge(self.ctx(), ctx1.const_ctx()) };
+                if res == BLST_ERROR::BLST_SUCCESS {
+                    Ok(())
+                } else {
+                    Err(res)
+                }
+            }
+
+            pub fn finalverify(&self, gtsig: Option<&Fp12>) -> bool {
+                unsafe {
+                    blst_pairing_finalverify(
+                        self.const_ctx(),
+                        match gtsig {
+                            Some(ref gtsig) => &gtsig.0,
+                            None => std::ptr::null(),
+                        },
+                    )
+                }
+            }
+        }
+    };
+}
+
+impl_pairing!(
+    PairingG1G2,
+    G1Affine,
+    G2Affine,
+    blst_pairing_aggregate_pk_in_g1,
+    blst_aggregated_in_g2
+);
+impl_pairing!(
+    PairingG2G1,
+    G2Affine,
+    G1Affine,
+    blst_pairing_aggregate_pk_in_g2,
+    blst_aggregated_in_g1
+);
